59eefd7022209d7174bcb6bc95e54d9b3c390b50
SimonAi1801 <56634821+SimonAi1801@users.noreply.github.com>
2020-09-29 21:24:34 +0200

    Add project files.

---
+++ MemberAdmin.Core/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MemberAdmin.Core
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _belt;
+        private DateTime _birthDate;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(_firstName);
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(_lastName);
+            }
+        }
+        public DateTime BirthDate
+        {
+            get
+            {
+                return _birthDate;
+            }
+            set
+            {
+                _birthDate = value;
+                OnPropertyChanged(_birthDate.ToString());
+            }
+        }
+
+
+        public string Belt 
+        { 
+            get 
+            {
+                return _belt;
+            }
+            set 
+            {
+                _belt = value;
+                OnPropertyChanged(_belt);
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                OnPropertyChanged($"{_firstName} {_lastName}");
+                return $"{_firstName} {_lastName}";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MemberAdmin.Core/Repository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Utils;
+
+namespace MemberAdmin.Core
+{
+    public class Repository
+    {
+        private const string _fileName = "members.csv";
+
+        private static Repository _instance;
+
+        List<Person> _members;
+
+        public Repository()
+        {
+            _members = new List<Person>();
+            LoadFromCsv();
+        }
+
+        private void LoadFromCsv()
+        {
+            string filePath = MyString.GetFullNameInApplicationTree(_fileName);
+            string[] lines = File.ReadAllLines(filePath);
+            if (lines != null)
+            {
+                foreach (var item in lines)
+                {
+                    string[] parts = item.Split(";");
+                    Person member = new Person
+                    {
+                        FirstName = parts[0],
+                        LastName = parts[1],
+                        BirthDate = Convert.ToDateTime(parts[2]),
+                        Belt = parts[3]
+                    };
+                    _members.Add(member);
+                }
+            }
+        }
+
+        public void Add(Person member)
+        {
+            _members.Add(member);
+        }
+
+        public void Remove(Person member)
+        {
+            _members.Remove(member);
+        }
+
+        public IEnumerable<Person> GetAll()
+        => _members.ToArray();
+    }
+}
