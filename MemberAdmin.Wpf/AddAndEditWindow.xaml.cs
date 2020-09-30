20fb66299ed7e174d3494b99563e80995fae9702
SimonAi1801 <56634821+SimonAi1801@users.noreply.github.com>
2020-09-30 16:17:10 +0200

    implemented wpf windows

---
+++ MemberAdmin.Wpf/AddAndEditWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MemberAdmin.Wpf
+{
+    /// <summary>
+    /// Interaction logic for AddAndEditWindow.xaml
+    /// </summary>
+    public partial class AddAndEditWindow : Window
+    {
+        public AddAndEditWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
