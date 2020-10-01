8f4894cd85fef09421cb9705d50af79535df911b
SimonAi1801 <56634821+SimonAi1801@users.noreply.github.com>
2020-10-01 17:46:30 +0200

    did improvements

--- MemberAdmin.Core/Person.cs
+++ MemberAdmin.Core/Person.cs
@@ -5,79 +5,16 @@ using System.Text;
 
 namespace MemberAdmin.Core
 {
-    public class Person : INotifyPropertyChanged
+    public class Person
     {
-        private string _firstName;
-        private string _lastName;
-        private string _belt;
-        private DateTime _birthDate;
+        public string FirstName { get; set; }
 
-        public string FirstName
-        {
-            get
-            {
-                return _firstName;
-            }
-            set
-            {
-                _firstName = value;
-                OnPropertyChanged(_firstName);
-            }
-        }
+        public string LastName { get; set; }
 
-        public string LastName
-        {
-            get
-            {
-                return _lastName;
-            }
-            set
-            {
-                _lastName = value;
-                OnPropertyChanged(_lastName);
-            }
-        }
-        public DateTime BirthDate
-        {
-            get
-            {
-                return _birthDate;
-            }
-            set
-            {
-                _birthDate = value;
-                OnPropertyChanged(_birthDate.ToString());
-            }
-        }
+        public DateTime BirthDate { get; set; }
 
+        public string Belt { get; set; }
 
-        public string Belt 
-        { 
-            get 
-            {
-                return _belt;
-            }
-            set 
-            {
-                _belt = value;
-                OnPropertyChanged(_belt);
-            }
-        }
-
-        public string FullName
-        {
-            get
-            {
-                OnPropertyChanged($"{_firstName} {_lastName}");
-                return $"{_firstName} {_lastName}";
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
